c1ab14eba288b1313b13749ed62bd5550a941a0f
Raphaël ALLAIS <allais.raphael@free.fr>
2023-03-20 15:36:54 +0100

    ajout matomo

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 			<meta name="category" content="teaching">
 			<meta name="description" content="Illustrations 3D des principale tolérances géométrique, d'après la norme du 'Geometrical Product Specification' (GPS)." />
 			<link  href="sources/style.css" rel="stylesheet"/>
-			<?php include_once("sources/Google_Analytics.php") ?>
+			<?php //include_once("sources/Google_Analytics.php") ?>
+			<?php include_once("sources/matomo.php"); ?>
 		</head>
 	<body>
 	
@@ -21,28 +22,28 @@
 
 				<div class="listeBoutons">
 					<div class="boutonMenu">
-						<a href="-cylindricite">
+						<a href="-cylindricite" onclick="_paq.push(['trackEvent', 'modèle',  'cylindricité']);">
 							Tolérance de cylindricité<br/>
 							<img src="models/cylindricite/dessin.png"/>
 						</a>
-						<a href="~cylindricite">
+						<!--<a href="~cylindricite">
 							<img style="vertical-align:middle;" alt="VR" src="./sources/images/casqueVR.png"/> <span style="vertical-align:middle">VR (pas encore fonctionnel)</span>
-						</a> 
+						</a> -->
 					</div>
 					<div class="boutonMenu">
-						<a href="-circularite">
+						<a href="-circularite" onclick="_paq.push(['trackEvent', 'modèle',  'circularité']);">
 							Tolérance de circularité<br/>
 							<img src="models/circularite/dessin.png"/>
 						</a>
 					</div>
 					<div class="boutonMenu">
-						<a href="-planeite">
+						<a href="-planeite" onclick="_paq.push(['trackEvent', 'modèle',  'planéité']);">
 							Tolérance de planéité<br/>
 							<img src="models/planeite/dessin.png"/>
 						</a>
 					</div>
 					<div class="boutonMenu">
-						<a href="-rectitude">
+						<a href="-rectitude" onclick="_paq.push(['trackEvent', 'modèle',  'rectitude']);">
 							Tolérance de rectitude<br/>
 							<img src="models/rectitude/dessin.png"/>
 						</a>
@@ -53,13 +54,13 @@
 
 				<div class="listeBoutons">
 					<div class="boutonMenu">
-						<a href="-parallelisme">
+						<a href="-parallelisme" onclick="_paq.push(['trackEvent', 'modèle',  'parallélisme']);">
 							Tolérance de parallélisme<br/>
 							<img src="models/parallelisme/dessin.png"/>
 						</a>
 					</div>
 					<div class="boutonMenu">
-						<a href="-perpendicularite">
+						<a href="-perpendicularite" onclick="_paq.push(['trackEvent', 'modèle',  'perpendicularité']);">
 							Tolérance de perpendicularité<br/>
 							<img src="models/perpendicularite/dessin.png"/>
 						</a>
@@ -70,19 +71,19 @@
 
 				<div class="listeBoutons">
 					<div class="boutonMenu">
-						<a href="-coaxialite">
+						<a href="-coaxialite" onclick="_paq.push(['trackEvent', 'modèle',  'coaxialité']);">
 							Tolérance de coaxialité<br/>
 							<img src="models/coaxialite/dessin.png"/>
 						</a>
 					</div>
 					<div class="boutonMenu">
-						<a href="-symetrie">
+						<a href="-symetrie" onclick="_paq.push(['trackEvent', 'modèle',  'symétrie']);">
 							Tolérance de symétrie<br/>
 							<img src="models/symetrie/dessin.png"/>
 						</a>
 					</div>
 					<div class="boutonMenu">
-						<a href="-localisation">
+						<a href="-localisation" onclick="_paq.push(['trackEvent', 'modèle',  'localisation']);">
 							Tolérance de localisation<br/>
 							<img src="models/localisation/dessin.png"/>
 						</a>
@@ -96,7 +97,7 @@
 			
 				<div class="listeBoutons">
 					<div class="boutonMenu">
-						<a href="levier-de-platine.php">
+						<a href="levier-de-platine.php" onclick="_paq.push(['trackEvent', 'modèle',  'leivier de platine']);">
 							<img src="./sources/images/bouton-levier-platine.png"/>
 						</a>
 					</div>
@@ -109,7 +110,7 @@
 			
 				<div class="listeBoutons">
 					<div class="boutonMenu">
-						<a href="rail_de_guidage.php">
+						<a href="rail_de_guidage.php" onclick="_paq.push(['trackEvent', 'modèle',  'rail de guidage']);">
 							<img src="./sources/images/bouton-rail-de-guidage.png"/>
 						</a>
 					</div>
@@ -122,7 +123,7 @@
 			
 				<div class="listeBoutons">
 					<div class="boutonMenu">
-						<a href="joue_inferieure.php">
+						<a href="joue_inferieure.php" onclick="_paq.push(['trackEvent', 'modèle',  'joue inférieure']);">
 							<img src="./sources/images/bouton_joue_inferieure.png"/>
 						</a>
 					</div>
---
+++ sources/matomo.php
@@ -0,0 +1,15 @@
+<!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.allais.eu/";
+    _paq.push(['setTrackerUrl', u+'tomatophp']);
+    _paq.push(['setSiteId', '6']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'tomatojs'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
